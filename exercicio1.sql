1766e546032b7a4624fccc8fe829128bac8dab68
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2024-08-21 23:55:17 -0300

    Update exercicio1.sql

--- exercicio1.sql
+++ exercicio1.sql
@@ -13,3 +13,11 @@ select * from Mercado;
 set identity_insert dbo.mercado on; --
 
 insert into mercado (ID,Produto,Preco,Descricao) values (1 ,'Processador i3' , 798.99 , 'INTEL CORE I3-14100F');
+insert into mercado (ID,Produto,Preco,Descricao) values (2 , 'Processador i5' , 1468.99 , 'INTEL CORE I5-14400F');
+insert into mercado (ID,Produto,Preco,Descricao) values (3 ,'Processador i7' , 2698.99 , 'INTEL CORE I7-14700');
+insert into mercado (ID,Produto,Preco,Descricao) values (4 ,'Processador i7' , 5199.99 , 'INTEL CORE I9-14900KS');
+insert into mercado (ID,Produto,Preco,Descricao) values (5 ,'PLACA MAE GIGABYTE' , 879.99 , 'B760M GAMING AC');
+insert into mercado (ID,Produto,Preco,Descricao) values (6 ,'PLACA MAE ASUS' , 2499.99 , 'TUF GAMING Z790-BTF WIFI');
+insert into mercado (ID,Produto,Preco,Descricao) values (7 ,'PLACA MAE MSI' , 1499.99 , 'MAG B760M MORTAR WIFI II');
+insert into mercado (ID,Produto,Preco,Descricao) values (8 ,'PLACA MAE ASROCK' , 2899.99 , 'Z790 TAICHI LITE, DDR5');
+insert into mercado (ID,Produto,Preco,Descricao) values (9 ,'PLACA MAE GIGABYTE' , 2299.99, 'Z790 AORUS ELITE X, DDR5')
