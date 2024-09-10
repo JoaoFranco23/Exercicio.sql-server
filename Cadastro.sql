d9409f911c6b428826e0c28bebeeba92a122c785
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2024-09-10 01:29:53 -0300

    Update Cadastro.sql

--- Cadastro.sql
+++ Cadastro.sql
@@ -6,8 +6,15 @@ go
 create table Cadastro(
 	ID int identity(1, 1) not null primary key,
 	Nome varchar(100),
+	Data_Nasc date,
+	Idade int,
 	Endereco varchar(100),
 	Bairro varchar(50),
 	Cidade varchar(50),
 	Estado varchar(20),
+	Email varchar(100),
+	Senha varbinary(8000),
+	check (Idade >=18 and Data_Nasc = 'AAAA-MM-DD')
 );
+
+select * from Cadastro;
